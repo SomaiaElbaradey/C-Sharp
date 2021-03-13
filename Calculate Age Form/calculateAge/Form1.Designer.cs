469dc651a773b44ca6ffd7825f881691f159c819
SomaiaElbaradey <somayaelbaradey@gmail.com>
2021-03-13 23:16:48 +0200

    add calculate age form

---
+++ Calculate Age Form/calculateAge/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace calculateAge
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ddlYear = new System.Windows.Forms.ComboBox();
+            this.ddlMonth = new System.Windows.Forms.ComboBox();
+            this.dllDay = new System.Windows.Forms.ComboBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.Location = new System.Drawing.Point(368, 310);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(302, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Year:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(302, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Month:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Location = new System.Drawing.Point(302, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Day:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Purple;
+            this.label4.Location = new System.Drawing.Point(316, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Calculate Your Age";
+            // 
+            // ddlYear
+            // 
+            this.ddlYear.FormattingEnabled = true;
+            this.ddlYear.Location = new System.Drawing.Point(395, 98);
+            this.ddlYear.Name = "ddlYear";
+            this.ddlYear.Size = new System.Drawing.Size(151, 28);
+            this.ddlYear.TabIndex = 8;
+            this.ddlYear.SelectedIndexChanged += new System.EventHandler(this.ddlYear_SelectedIndexChanged);
+            // 
+            // ddlMonth
+            // 
+            this.ddlMonth.FormattingEnabled = true;
+            this.ddlMonth.Location = new System.Drawing.Point(395, 163);
+            this.ddlMonth.Name = "ddlMonth";
+            this.ddlMonth.Size = new System.Drawing.Size(151, 28);
+            this.ddlMonth.TabIndex = 9;
+            this.ddlMonth.SelectedIndexChanged += new System.EventHandler(this.ddlMonth_SelectedIndexChanged);
+            // 
+            // dllDay
+            // 
+            this.dllDay.FormattingEnabled = true;
+            this.dllDay.Location = new System.Drawing.Point(395, 226);
+            this.dllDay.Name = "dllDay";
+            this.dllDay.Size = new System.Drawing.Size(151, 28);
+            this.dllDay.TabIndex = 10;
+            this.dllDay.SelectedIndexChanged += new System.EventHandler(this.dllDay_SelectedIndexChanged);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelResult.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.labelResult.Location = new System.Drawing.Point(230, 389);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(17, 25);
+            this.labelResult.TabIndex = 11;
+            this.labelResult.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.dllDay);
+            this.Controls.Add(this.ddlMonth);
+            this.Controls.Add(this.ddlYear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox ddlYear;
+        private System.Windows.Forms.ComboBox ddlMonth;
+        private System.Windows.Forms.ComboBox dllDay;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ Calculate Age Form/calculateAge/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculateAge
+{
+    public partial class Form1 : Form
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public string Result { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            fillYear();
+        }
+        private void fillYear()
+        {
+            IEnumerable<int> year = Enumerable.Range(1891, 131);
+            foreach (var item in year)
+            {
+                ddlYear.Items.Add(item);
+            }
+        }
+        private void ddlMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Month = int.Parse(ddlMonth.Text);
+            if(Year != 0) fillDays();
+        }
+        private void ddlYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Year = int.Parse(ddlYear.Text);
+            ddlMonth.Items.Clear();
+            IEnumerable<int> months;
+            if (Year == 2021) months = Enumerable.Range(1, DateTime.Today.Month);
+            else months = Enumerable.Range(1, 12);
+            foreach (var item in months)
+            {
+                ddlMonth.Items.Add(item);
+            }
+        }
+        private void dllDay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Day = int.Parse(dllDay.Text);
+        }
+        private void fillDays()
+        {
+            dllDay.Items.Clear();
+            IEnumerable<int> days;
+            if (Year == 2021 && Month == DateTime.Today.Month) days = Enumerable.Range(1, DateTime.Today.Day);
+            else
+            {
+                int y;
+                if (Month == 2)
+                {
+                    if ((Year % 400) == 0) y = 29;
+                    else if ((Year % 100) == 0) y = 28;
+                    else if ((Year % 4) == 0) y = 29;
+                    else y = 28;
+                }
+                else if (Month == 4 || Month == 6 || Month == 9 || Month == 11) y = 30;
+                else y = 31;
+                days = Enumerable.Range(1, y);
+            }
+            foreach (var item in days)
+            {
+                dllDay.Items.Add(item);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime birthDate = new DateTime(Year, Month, Day);
+            DateTime today = DateTime.Today;
+            int totalDays = today.Subtract(birthDate).Days;
+            int totalYears = totalDays / 365;
+            int totalMonths = (totalDays % 365) / 30;
+            int remainingDays = (totalDays % 365) % 30;
+            Result = $"you have {totalYears} years, {totalMonths} months, and {remainingDays} days old";
+            labelResult.Text = Result;
+        }
+    }
+}
